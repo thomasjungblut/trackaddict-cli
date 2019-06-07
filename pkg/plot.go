af0682674447982cf087405e65af9180c57fb997
Heiko Boomgaarden <boomgaarden.heiko@gmail.com>
2019-06-07 18:09:18 +0200

    added coloring and detection of laps in plotting

--- pkg/plot.go
+++ pkg/plot.go
@@ -5,29 +5,37 @@ import (
 	"github.com/fogleman/gg"
 	"github.com/golang/geo/s2"
 	"image/color"
+	"math/rand"
 )
 
 func Plot(inputFile string, outputFile string) error {
-	_, measures, err := readTrackMeasures(inputFile)
+	trackInfo, measures, err := readTrackMeasures(inputFile)
 	if err != nil {
 		return err
 	}
 
-	plot(measures, outputFile)
-	return nil
+	laps := extractLaps(measures, trackInfo)
+
+	return plot(measures, laps, outputFile)
 }
 
-func plot(measures []GPSMeasurement, outputFile string) error {
+func plot(measures []GPSMeasurement, laps []Lap, outputFile string) error {
 	println("Plotting your map...")
 	ctx := sm.NewContext()
 	ctx.SetSize(2000, 2000)
 
-	positions := make([]s2.LatLng, len(measures))
-	for i := 0; i < len(measures); i++ {
-		positions[i] = s2.LatLngFromDegrees(measures[i].latLng[0], measures[i].latLng[1])
+		for lapNum := 0; lapNum < len(laps); lapNum++ {
+			lapSet := measures[laps[lapNum].measureStartIndex : laps[lapNum].measureEndIndexExclusive]
+			positions := make([]s2.LatLng, len(lapSet))
+
+			for i := 0; i < len(lapSet); i++ {
+				positions[i] = s2.LatLngFromDegrees(lapSet[i].latLng[0], lapSet[i].latLng[1])
+			}
+			lapPath := sm.NewPath(positions, color.RGBA{uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), 0xff}, 2.0)
+			ctx.AddPath(lapPath)
 	}
-	path := sm.NewPath(positions, color.RGBA{0xff, 0, 0, 0xff}, 2.0)
-	ctx.AddPath(path)
+
+
 	img, err := ctx.Render()
 	if err != nil {
 		return err
